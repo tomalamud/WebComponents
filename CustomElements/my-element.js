6a18adf51238d3ff6a81e926f7913fa102d2b562
Tomás Elizondo <tomaslelizondo@gmail.com>
2022-04-28 16:32:37 -0300

    [BREAKING CHANGE] template for web components

---
+++ CustomElements/my-element.js
@@ -0,0 +1,41 @@
+class myElement extends HTMLElement {
+  constructor() { // inicializa el componente
+    super(); // para tener acceso a los métodos de la clase de la que extiende
+    this.attachShadow({ mode: 'open'})
+  }
+  getTemplate() {
+    const template = document.createElement('template');
+    template.innerHTML = `
+      <section>
+        <h2 class="holamundo">Hola Mundo</h2>
+        <div>
+          <p>Este es un texto de prueba</p>
+        </div>
+      </section>
+      ${this.getStyles()}
+    `;
+    return template;
+  }
+  getStyles() {
+    return `
+      <style>
+        h2 {
+          color: #4dd331;
+        }
+        p {
+          color: blue;
+        }
+      </style>
+    `;
+  }
+  render() {
+    // una vez que en el constructor se inicializa el shadow dom, 
+    // en vez de interactuar con el componente a través del document,
+    // ahora hay que interactuar con él por medio del shadowRoot.
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true)); // true = clonar todos los elementos anidados dentro del nodo
+  }
+  connectedCallback() { // Lo carga como un nodo en el DOM
+    this.render()
+  }
+}
+customElements.define('my-element', myElement); // customElements viene de HTMLElements
